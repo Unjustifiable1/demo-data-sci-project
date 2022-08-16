be34d52708d0565a35598655d4341b5051f1780c
Unjustifiable1 <49613319+Unjustifiable1@users.noreply.github.com>
2022-08-16 20:15:19 +0200

    add a placeholder function for Mike.

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
+from readline import append_history_file
+
+
 print("Hello World!!")
 print(3*"Hi there!! ")
+
+d ={}
+d["app - key"] = "apples - apples - hello world"
+d["bin - key"] = "bins - bins - helloworld"
+d["dog - key"] = "dogs"
+d["ele - key"] = "elephants"
+
+def my_function():
+    pass
+
+def sarahs_function():
+    pass
